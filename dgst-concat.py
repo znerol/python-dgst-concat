4b803f88a8fb84ae87431b991772dda2aeb773f9
znerol <lo+github@znerol.ch>
2020-01-17 15:32:35 +0100

    Hide stack traces by default, reveal them with -d option

--- dgst-concat.py
+++ dgst-concat.py
@@ -42,7 +42,7 @@ class DigestParser(object):
                        flag=result.group('flag'),
                        path=result.group('path'))
             else:
-                raise DigestParserError(f'Unexpected line {line}')
+                raise DigestParserError(f'Unexpected line "{line}'.rstrip("\r\n") + '"')
 
 
 class DigestList(object):
@@ -68,18 +68,29 @@ class DigestList(object):
                                 flag=entry.flag,
                                 path=dirname.joinpath(entry.path))
                 except DigestParserError as e:
-                    raise DigestFileError(f'Failed while parsing {dgstfile}') from e
+                    raise DigestFileError(f'Failed while parsing "{dgstfile}", {e}') from e
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Recursively concatenate coreutils digest files.')
     parser.add_argument('patterns', metavar='PATTERN', type=str, nargs='+',
             help='glob pattern for digest files. E.g., "**/*.md5"')
+    parser.add_argument('-d', '--debug', action="store_true",
+            help='print a stacktrace when something goes wrong')
     parser.add_argument('-o', '--outfile',
-            type=argparse.FileType('w', encoding='UTF-8'), nargs='?',
+            type=argparse.FileType('w', encoding='UTF-8'),
             help='output file, defaults to standard output', default=sys.stdout)
 
     args = parser.parse_args()
+
+    def exception_handler(exception_type, exception, traceback, debug_hook=sys.__excepthook__):
+        if args.debug:
+            debug_hook(exception_type, exception, traceback)
+        else:
+            print(f'{exception_type.__name__}: {exception}')
+
+    sys.excepthook = exception_handler
+
     for pattern in args.patterns:
         for entry in DigestList().join(Path('.').glob(pattern)):
             print(f'{entry.digest} {entry.flag}{entry.path}', file=args.outfile)
