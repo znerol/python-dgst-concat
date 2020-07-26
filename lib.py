161c3cb8eb86070741ec1837ccf2e441cb271840
znerol <lo+github@znerol.ch>
2020-07-26 15:30:49 +0200

    Use PureUnixPath / PureWindowsPath in DigestEntry and autodetect input file format

--- lib.py
+++ lib.py
@@ -1,5 +1,8 @@
+import io
+import os
 import re
 from collections import namedtuple
+from pathlib import PurePath, PurePosixPath, PureWindowsPath
 
 
 DigestEntry = namedtuple('DigestEntry', ['digest', 'flag', 'path'])
@@ -13,8 +16,71 @@ class DigestParserError(DigestError):
     pass
 
 
+class DigestFormatError(DigestError):
+    pass
+
+
 class DigestFileError(DigestError):
     pass
+
+
+class DigestFormat(object):
+    """
+    Represents a digest text file format used to differentiate between POSIX
+    und WINDOWS line- and path separators.
+    """
+
+    def __init__(self, linesep: str, pathcls: PurePath):
+        self._linesep = linesep
+        self._pathcls = pathcls
+
+    def match(self, buf: io.BufferedReader) -> bool:
+        """
+        Return true if the file represented by the given buffer has the desired
+        line separation.
+        """
+        chunk = buf.peek()
+        return self._linesep.encode() in chunk
+
+    def text(self, buf: io.BufferedReader) -> None:
+        """
+        Generates a list of text lines from the given `buf` with line separator
+        stripped.
+        """
+        with io.TextIOWrapper(buf, newline=self._linesep) as text:
+            for line in text:
+                yield line.rstrip(self._linesep)
+
+    def path(self, path: str) -> PurePath:
+        """
+        Returns either a PurePosixPath or a PureWindowsPath according to the
+        actual file format.
+        """
+        return self._pathcls(path)
+
+
+class DigestFormats(object):
+    """
+    Implements a simple mechanism to guess whether a digest file is in POSIX or
+    WINDOWS format.
+    """
+
+    NATIVE = DigestFormat(os.linesep, PurePath)
+    UNIX = DigestFormat('\n', PurePosixPath)
+    WINDOWS = DigestFormat('\r\n', PureWindowsPath)
+
+    candidates = [WINDOWS, UNIX]
+
+    def guess(self, buf: io.BufferedReader) -> DigestFormat:
+        """
+        Returns either DigestFormats.WINDOWS or DigestFormats.UNIX
+        """
+        for candidate in self.candidates:
+            if candidate.match(buf):
+                return candidate
+        else:
+            raise DigestFormatError(f'Failed to detect DOS or UNIX line '
+                                    'separator')
 
 
 class DigestParser(object):
@@ -28,7 +94,7 @@ class DigestParser(object):
     """
     pattern = re.compile(r'(?P<digest>[0-9A-Fa-f]+) (?P<flag>[\* ])(?P<path>.*)')
 
-    def parse(self, lines):
+    def parse(self, lines, fmt: DigestFormat = DigestFormats.NATIVE):
         """
         Iterates through the list of lines and yields a DigestEntry for each of
         them. Throws a RuntimeException if a line does not match the expected
@@ -40,20 +106,25 @@ class DigestParser(object):
                 yield DigestEntry(
                        digest=result.group('digest'),
                        flag=result.group('flag'),
-                       path=result.group('path'))
+                       path=fmt.path(result.group('path')))
             else:
-                raise DigestParserError(f'Unexpected line "{line}'.rstrip("\r\n") + '"')
+                raise DigestParserError(f'Unexpected line "{line}"')
 
 
 class DigestList(object):
     """
     Iterate through a list of coreutils digest files.
     """
-    parser = DigestParser()
 
-    def __init__(self, flat=False, flag=None):
+    def __init__(self,
+                 flat=False,
+                 flag=None,
+                 parser: DigestParser = DigestParser(),
+                 formats: DigestFormats = DigestFormats()):
         self.flat = flat
         self.flag = flag
+        self.parser = parser
+        self.formats = formats
 
     def join(self, paths):
         """
@@ -64,12 +135,15 @@ class DigestList(object):
         """
         for dgstfile in paths:
             dirname = dgstfile.parent
-            with dgstfile.open() as lines:
+            with dgstfile.open('rb') as buf:
                 try:
-                    for entry in self.parser.parse(lines):
+                    fmt = self.formats.guess(buf)
+                    for entry in self.parser.parse(fmt.text(buf), fmt):
                         yield DigestEntry(
                                 digest=entry.digest,
                                 flag=entry.flag if self.flag is None else self.flag,
                                 path=entry.path if self.flat else dirname / entry.path)
+                except DigestFormatError as e:
+                    raise DigestFileError(f'Failed while opening "{dgstfile}", {e}') from e
                 except DigestParserError as e:
                     raise DigestFileError(f'Failed while parsing "{dgstfile}", {e}') from e
