dd1ab1155364e09ca951204df5cfedf078e59757
znerol <lo+github@znerol.ch>
2026-07-03 14:42:39 +0200

    Fix variable name

--- lib.py
+++ lib.py
@@ -223,8 +223,8 @@ class DigestList(object):
             with dgstfile.open('rb') as buf:
                 try:
                     # Skip UTF-8 BOM if any.
-                    if dgstfile.peek(3) == b'\xef\xbb\xbf':
-                        dgstfile.seek(3)
+                    if buf.peek(3) == b'\xef\xbb\xbf':
+                        buf.seek(3)
                     filefmt = self.filefmts.guess(buf)
                     linefmt = self.linefmts.guess(buf)
                     entries = self.parser.parse(
